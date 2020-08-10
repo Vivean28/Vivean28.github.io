5ccb63d68e7f15709512e1ff3a46b2becb7f8e2a
Vivean28 <68155629+Vivean28@users.noreply.github.com>
2020-08-09 17:09:18 -0700

    Add files via upload

---
+++ treasurehunt.js
@@ -0,0 +1,42 @@
+
+var rainbow = Math.floor(Math.random() * 9);
+var bomb = Math.floor(Math.random() * 9);
+while (rainbow === bomb) {
+  bomb = Math.floor(Math.random() * 9)
+}
+console.log(rainbow, bomb);
+
+
+var count = 0;
+const treasure = (location) => {
+  var counter = count++
+  document.getElementById("counter").innerHTML = "# of clicks: " + count;
+  console.log(count);
+  if (location == rainbow){
+    //WINNING CONDITION!
+    document.getElementById(location).innerHTML = "&#x1f308";
+    document.getElementById("gameBoard").style.pointerEvents = "none";
+    setTimeout(function () {
+      alert('your rich!');
+    }, 500);
+  }
+  //LOSING CONDITION
+  else if (location == bomb){
+    document.getElementById(location).innerHTML = "&#x2620";
+    document.getElementById("gameBoard").style.pointerEvents = "none";
+    setTimeout(function () {
+      alert('You ded...');
+    }, 500);
+  }
+  //NEUTRAL CONDITION
+  else {
+    document.getElementById(location).innerHTML = "&#x1f332";
+    document.getElementById(location).style.pointerEvents = "none";
+    if (counter == 5){
+      document.getElementById("gameBoard").style.pointerEvents = "none";
+      setTimeout(function () {
+        alert('Your out of turns!');
+      }, 500);
+    }
+  }
+}
